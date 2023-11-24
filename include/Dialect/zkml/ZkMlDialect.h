e9444964b2aae85bdfe37251b153f84b8751d3f9
Franco Nieddu <nieddu@taceo.io>
2023-11-24 15:57:53 +0100

    feat: init commit

---
+++ include/Dialect/zkml/ZkMlDialect.h
@@ -0,0 +1,10 @@
+#ifndef LIB_DIALECT_ZKML_DIALCET_H_
+#define LIB_DIALECT_ZKML_DIALCET_H_
+
+// Required because the .h.inc file refers to MLIR classes and does not itself
+// have any includes.
+#include "mlir/IR/DialectImplementation.h"
+
+#include "ZkMlDialect.h.inc"
+
+#endif  // LIB_DIALECT_ZKML_DIALCET_H_
---
+++ src/Dialect/zkml/ZkMlDialect.cpp
@@ -0,0 +1,23 @@
+#include "Dialect/zkml/ZkMlDialect.h"
+#include "Dialect/zkml/Ops/Gather/Gather.h"
+
+#include "mlir/IR/Builders.h"
+#include "llvm/ADT/TypeSwitch.h"
+
+#include "ZkMlDialect.cpp.inc"
+#define GET_OP_CLASSES
+#include "IR/Gather/Gather.cpp.inc"
+
+namespace zk_ml_toolchain {
+namespace zkml {
+
+void ZkMlDialect::initialize() {
+  // This is where we will register types and operations with the dialect
+addOperations<
+#define GET_OP_LIST
+#include "IR/Gather/Gather.cpp.inc"
+      >();
+};
+
+}  // namespace tutorial
+}  // namespace mlir
