076091b1af776874156d02ead087bc3aaf0e471e
fnieddu <118167989+fnieddu@users.noreply.github.com>
2023-11-28 12:00:28 +0100

    feat: dot product (#2)
    * removed unnecassary pub lib links, add DotProduct.td
    * Fixed DotProduct output

--- src/mlir/Dialect/zkml/ZkMlDialect.cpp
+++ src/mlir/Dialect/zkml/ZkMlDialect.cpp
@@ -1,5 +1,6 @@
 #include "mlir/Dialect/zkml/ZkMlDialect.h"
 #include "mlir/Dialect/zkml/IR/Gather.h"
+#include "mlir/Dialect/zkml/IR/DotProduct.h"
 
 #include "mlir/IR/Builders.h"
 #include "llvm/ADT/TypeSwitch.h"
@@ -7,6 +8,9 @@
 #include "mlir/Dialect/zkml/ZkMlDialect.cpp.inc"
 #define GET_OP_CLASSES
 #include "mlir/Dialect/zkml/IR/Gather.cpp.inc"
+
+#define GET_OP_CLASSES
+#include "mlir/Dialect/zkml/IR/DotProduct.cpp.inc"
 
 namespace mlir {
 namespace zkml {
@@ -16,6 +20,11 @@ void ZkMlDialect::initialize() {
 addOperations<
 #define GET_OP_LIST
 #include "mlir/Dialect/zkml/IR/Gather.cpp.inc"
+      >();
+
+addOperations<
+#define GET_OP_LIST
+#include "mlir/Dialect/zkml/IR/DotProduct.cpp.inc"
       >();
 };
 
