7eccfae62f1f43deb84c7c320ebb54769c95f160
Franco Nieddu <nieddu@taceo.io>
2024-01-24 14:00:51 +0100

    feat: added public/private attributes

---
+++ include/mlir/Dialect/zkml/IR/ZkMlAttributes.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/OpDefinition.h"
+
+#define GET_ATTRDEF_CLASSES
+#include "mlir/Dialect/zkml/IR/ZkMlAttrDefs.h.inc"
--- src/mlir/Dialect/zkml/ZkMlDialect.cpp
+++ src/mlir/Dialect/zkml/ZkMlDialect.cpp
@@ -5,6 +5,7 @@
 #include "mlir/Dialect/zkml/IR/DotProduct.h"
 #include "mlir/Dialect/zkml/IR/Trace.h"
 #include "mlir/Dialect/zkml/IR/OnnxAmount.h"
+#include "mlir/Dialect/zkml/IR/ZkMlAttributes.hpp"
 
 #include "mlir/IR/Builders.h"
 #include "llvm/ADT/TypeSwitch.h"
@@ -29,39 +30,60 @@
 #define GET_OP_CLASSES
 #include "mlir/Dialect/zkml/IR/OnnxAmount.cpp.inc"
 
-namespace mlir {
-namespace zkml {
+#define GET_ATTRDEF_CLASSES
+#include "mlir/Dialect/zkml/IR/ZkMlAttrDefs.cpp.inc"
 
-void ZkMlDialect::initialize() {
-  // This is where we will register types and operations with the dialect
-addOperations<
+namespace mlir {
+    namespace zkml {
+
+        void ZkMlDialect::initialize() {
+            // This is where we will register types and operations with the dialect
+            addOperations<
 #define GET_OP_LIST
 #include "mlir/Dialect/zkml/IR/ArgMin.cpp.inc"
-      >();
-addOperations<
+                >();
+            addOperations<
 #define GET_OP_LIST
 #include "mlir/Dialect/zkml/IR/ArgMax.cpp.inc"
-      >();
-addOperations<
+                >();
+            addOperations<
 #define GET_OP_LIST
 #include "mlir/Dialect/zkml/IR/Gather.cpp.inc"
-      >();
+                >();
 
-addOperations<
+            addOperations<
 #define GET_OP_LIST
 #include "mlir/Dialect/zkml/IR/DotProduct.cpp.inc"
-      >();
+                >();
 
-addOperations<
+            addOperations<
 #define GET_OP_LIST
 #include "mlir/Dialect/zkml/IR/Trace.cpp.inc"
-      >();
+                >();
 
-addOperations<
+            addOperations<
 #define GET_OP_LIST
 #include "mlir/Dialect/zkml/IR/OnnxAmount.cpp.inc"
-      >();
-};
+                >();
 
-}  // namespace tutorial
-}  // namespace mlir
+            addAttributes<
+#define GET_ATTRDEF_LIST
+#include "mlir/Dialect/zkml/IR/ZkMlAttrDefs.cpp.inc"
+                >();
+        };
+
+        Attribute ZkMlDialect::parseAttribute(DialectAsmParser &parser, Type type) const {
+            StringRef attrTag;
+            if (Attribute attr; generatedAttributeParser(parser, &attrTag, type, attr).has_value())
+                return attr;
+            parser.emitError(parser.getCurrentLocation()) << "unknown attribute `" << attrTag << "` in dialect `zkML`";
+            return {};
+        };
+
+        /// Print an attribute registered to this dialect.
+        void ZkMlDialect::printAttribute(Attribute attr, DialectAsmPrinter &printer) const {
+            assert(succeeded(generatedAttributePrinter(attr, printer)) && "cannot print");
+        };
+
+    }    // namespace zkml
+}    // namespace mlir
